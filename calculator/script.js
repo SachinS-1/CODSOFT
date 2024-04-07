574e7dd651c6834c1b0b8c8ec99252a8237dfe8d
SachinS-1 <166314413+SachinS-1@users.noreply.github.com>
2024-04-07 23:18:33 +0530

    Update script.js

--- calculator/script.js
+++ calculator/script.js
@@ -1,121 +1,17 @@
-
-
-(function() {
-    "use strict";
-  
-   
-    var el = function(element) {
-      if (element.charAt(0) === "#") { 
-        return document.querySelector(element);
-      }
-  
-      return document.querySelectorAll(element); 
-    };
-  
-   
-    var viewer = el("#viewer"), 
-      equals = el("#equals"), 
-      nums = el(".num"), 
-      ops = el(".ops"), 
-      theNum = "", 
-      oldNum = "", 
-      resultNum,
-      operator; 
-  
- 
-    var setNum = function() {
-      if (resultNum) { 
-        theNum = this.getAttribute("data-num");
-        resultNum = "";
-      } else { 
-        theNum += this.getAttribute("data-num");
-      }
-  
-      viewer.innerHTML = theNum;
-  
-    };
-  
-  
-    var moveNum = function() {
-      oldNum = theNum;
-      theNum = "";
-      operator = this.getAttribute("data-ops");
-  
-      equals.setAttribute("data-result", ""); // Reset result in attr
-    };
-  
-    
-    var displayNum = function() {
-  
-     
-      oldNum = parseFloat(oldNum);
-      theNum = parseFloat(theNum);
-  
-   
-      switch (operator) {
-        case "plus":
-          resultNum = oldNum + theNum;
-          break;
-  
-        case "minus":
-          resultNum = oldNum - theNum;
-          break;
-  
-        case "times":
-          resultNum = oldNum * theNum;
-          break;
-  
-        case "divided by":
-          resultNum = oldNum / theNum;
-          break;
-  
-          // If equal is pressed without an operator, keep number and continue
-        default:
-          resultNum = theNum;
-      }
-  
-      // If NaN or Infinity returned
-      if (!isFinite(resultNum)) {
-        if (isNaN(resultNum)) {
-          resultNum = "You broke it!";
-        } else { 
-          resultNum = "Look at what you've done";
-          el('#calculator').classList.add("broken"); 
-          el('#reset').classList.add("show"); 
-        }
-      }
-  
-      // Display result, finally!
-      viewer.innerHTML = resultNum;
-      equals.setAttribute("data-result", resultNum);
-  
-     
-      oldNum = 0;
-      theNum = resultNum;
-  
-    };
-  
-    
-    var clearAll = function() {
-      oldNum = "";
-      theNum = "";
-      viewer.innerHTML = "0";
-      equals.setAttribute("data-result", resultNum);
-    };
-  
-   
-    for (var i = 0, l = nums.length; i < l; i++) {
-      nums[i].onclick = setNum;
-    }
-  
- 
-    for (var i = 0, l = ops.length; i < l; i++) {
-      ops[i].onclick = moveNum;
-    }
-  
-   
-    equals.onclick = displayNum;
-  
- 
-    el("#clear").onclick = clearAll;
-  }());
+function Solve(val) {
+  var v = document.getElementById('res');
+  v.value += val;
+}
+function Result() {
+  var num1 = document.getElementById('res').value;
+  var num2 = eval(num1);
+  document.getElementById('res').value = num2;
+}
+function Clear() {
+  var inp = document.getElementById('res');
+  inp.value = '';
+}
+function Back() {
+  var ev = document.getElementById('res');
+  ev.value = ev.value.slice(0,-1);
+}
